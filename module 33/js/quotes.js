79334340185b1c106d8b78a9321c27ee981b9b8a
webnur <mdnuruli291@gmail.com>
2022-08-27 21:20:19 -0700

    Basic API concept recap with Kanye West

---
+++ module 33/js/quotes.js
@@ -0,0 +1,12 @@
+
+const loadquote = () => {
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuote(data))
+}
+
+
+const displayQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+    blockQuote.innerText = quote.quote;
+}
