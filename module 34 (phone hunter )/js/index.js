c1125cc354ed89d11e98dd6d53702f20862eb935
webnur <mdnuruli291@gmail.com>
2022-08-30 05:53:11 +0600

    laod date using phone hunter api

---
+++ module 34 (phone hunter )/js/index.js
@@ -0,0 +1,17 @@
+
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data)
+}
+
+const displayPhones = phones => {
+    console.log(phones)
+
+    const phoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone => {
+        
+    })
+}
+loadPhones()
