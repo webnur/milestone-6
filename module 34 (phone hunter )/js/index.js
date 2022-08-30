500fa20b5298a0576cfbdd16539f3e52def21658
webnur <mdnuruli291@gmail.com>
2022-08-30 06:12:09 +0600

    Display phones and implement search functionality half done

--- module 34 (phone hunter )/js/index.js
+++ module 34 (phone hunter )/js/index.js
@@ -7,11 +7,27 @@ const loadPhones = async () => {
 }
 
 const displayPhones = phones => {
-    console.log(phones)
-
+  
     const phoneContainer = document.getElementById('phone-container')
     phones.forEach(phone => {
+        console.log(phone)
+        const {brand, phone_name, image, slug} = phone;
+
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
         
+            <div class="card h-100 p-4">
+                <img src="${image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone_name}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        
+        `;
+
+        phoneContainer.appendChild(phoneDiv)
     })
 }
 loadPhones()
