33c492c2ec7507cc390feb088d5829aadb8c9fdd
webnur <mdnuruli291@gmail.com>
2022-08-30 09:56:32 +0600

    Show and hide loading spinner while loading API data

--- module 34 (phone hunter )/js/index.js
+++ module 34 (phone hunter )/js/index.js
@@ -40,16 +40,33 @@ const displayPhones = phones => {
         
         `;
 
-        phoneContainer.appendChild(phoneDiv)
+        phoneContainer.appendChild(phoneDiv);
     })
+
+    // stop loader
+    toggleSpinner(false)
 }
 
+// handle search button click
 document.getElementById('btn-search').addEventListener('click', function(){
     
+    // start loader
+    toggleSpinner(true)
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhones(searchText)
     searchField.value = '';
 })
 
+
+const toggleSpinner = isLoading => {
+
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
+}
 // loadPhones()
